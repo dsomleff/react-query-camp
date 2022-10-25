d19b6d3fddbbb4ab2955e2bb7fe36a16eee95c29
somleff <somleff@gmail.com>
2022-10-25 13:37:59 +0200

    useInfiniteScroll

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,20 +1,22 @@
 import './App.css';
-import {QueryClient, QueryClientProvider} from "react-query";
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
 import { InfinitePeople } from './people/InfinitePeople';
 import { InfiniteSpecies } from './species/InfiniteSpecies';
 
+const queryClient = new QueryClient();
+
 function App() {
     return (
-        <QueryClientProvider client={QueryClient}>
-        <div className="App">
-            <h1>Infinite SWAPI</h1>
-            <InfinitePeople />
-            {/* <InfiniteSpecies /> */}
-            <ReactQueryDevtools/>
-        </div>
-    </QueryClientProvider>
+        <QueryClientProvider client={queryClient}>
+            <div className="App">
+                <h1>Infinite SWAPI</h1>
+                <InfinitePeople />
+                {/* <InfiniteSpecies /> */}
+                <ReactQueryDevtools />
+            </div>
+        </QueryClientProvider>
     );
 }
 
